d5a719816593236135469e3d1e5260ddd9e94764
Coursera Learner <nobody@nowhere.nul>
2020-11-28 08:43:05 +0000

    Assignment 19 C3.9

--- 19_bits_arr/numToBits.c
+++ 19_bits_arr/numToBits.c
@@ -1,11 +1,16 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+*/
+
 
 /*
  * Helper function that retrieves numbers 'bit' value
  * ie: 1's 31st bit is 1
  */
+
+/*
 int getNthBit(uint32_t number, int bit) {
   if (bit <0 || bit >= 32) {
     printf("Bit %d is invalid\n", bit);
@@ -13,6 +18,8 @@ int getNthBit(uint32_t number, int bit) {
   }
   return (number & (1<<bit)) != 0;
 }
+*/
+
 
 /*
  *
@@ -35,8 +42,58 @@ int getNthBit(uint32_t number, int bit) {
  *   That is, bits[0] should be bit 31 of nums[0], bits[1] should
  *   be bit 30 of nums[0], and so on.
  */
+
+/*
+
 void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
 
+}
+
+void doTest(uint32_t * nums, int n) {
+  int bits[n *32];
+  numToBits(nums, n, bits, n*32);
+  for (int i =0; i < n; i++) {
+    printf(" %9d (%8X) => ", nums[i], nums[i]);
+    for (int j = 0; j < 32; j++) {
+      printf("%d", bits[i*32 + j]);
+    }
+    printf("\n");
+  }
+}
+
+int main(void) {
+  uint32_t array1[] = { 1, 2, 3, 4, 5, 15, 109};
+  uint32_t array2[] = { 123456789, 987654321 };
+  int bits[7*32-1];
+  doTest (array1, 7);
+  doTest (array2, 2);
+  numToBits(array1,7, bits , 7*32-1);
+  return EXIT_SUCCESS;
+}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int getNthBit(uint32_t number, int bit) {
+  if (bit <0 || bit >= 32) {
+    printf("Bit %d is invalid\n", bit);
+    exit (EXIT_FAILURE);
+  }
+  return (number & (1<<bit)) != 0;
+}
+
+void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
+  if (nNums*32 > nBits){
+    printf("Invalid call to numToBits! nBits is %d, nNums is %d\n",nBits,nNums);
+    return;}
+  for (uint32_t i =0;i < nNums;i++){
+    for (int j=0 ; j <32 ;j ++){
+      bits[i*32 + j ]=getNthBit(nums[i],31-j);
+    }
+  }
+  return;
 }
 
 void doTest(uint32_t * nums, int n) {
