58f948767ab7b25e0dc3397fd258de3dde15d501
Coursera Learner <nobody@nowhere.nul>
2020-11-29 14:14:42 +0000

    C3prj1

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -78,7 +78,7 @@ void assert_full_deck(deck_t * d) {
   }
 }
 
-/*
+
 void add_card_to(deck_t * deck, card_t c){
 
   card_t* c1=malloc(sizeof(*c1));
@@ -186,7 +186,6 @@ void free_deck(deck_t * deck) {
   free(deck);
 
 }
-*/
 
 /*
 #include <stdio.h>
